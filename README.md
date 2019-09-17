93b0796fab065943be77ceba31afd0e3afaa2594
Denis Anisimov <vysheradugi4@gmail.com>
2019-09-17 14:31:08 -0400

    Readme completed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# go-cli
+# GO-CLI
+   
+   
+## The GO-CLI makes it easy to create an application that already works, right out of the box. It already follows our best practices!
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("    ██████╗  ██████╗        ██████╗██╗     ██╗")
+	fmt.Println("   ██╔════╝ ██╔═══██╗      ██╔════╝██║     ██║")
+	fmt.Println("   ██║  ███╗██║   ██║█████╗██║     ██║     ██║")
+	fmt.Println("   ██║   ██║██║   ██║╚════╝██║     ██║     ██║")
+	fmt.Println("   ╚██████╔╝╚██████╔╝      ╚██████╗███████╗██║")
+	fmt.Println("    ╚═════╝  ╚═════╝        ╚═════╝╚══════╝╚═╝")
+	fmt.Println("")
+	fmt.Println("")
+
+	mainCommand := os.Args[1]
+
+	if mainCommand == "new" {
+
+	}
+}
